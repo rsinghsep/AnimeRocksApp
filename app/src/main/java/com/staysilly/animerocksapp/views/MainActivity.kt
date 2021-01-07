20000efc39df8420c869dba5aa2ffeae75e9ae3c
rahul singh <rsinghsep@gmail.com>
2021-01-08 00:11:37 +0530

    added dependencies for retrofit in the and converted the layout file to wrap inside layotu tag

--- app/src/main/java/com/staysilly/animerocksapp/views/MainActivity.kt
+++ app/src/main/java/com/staysilly/animerocksapp/views/MainActivity.kt
@@ -2,13 +2,25 @@ package com.staysilly.animerocksapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.staysilly.animerocksapp.R
+import com.staysilly.animerocksapp.databinding.ActivityMainBinding
+import com.staysilly.animerocksapp.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var dataBinding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    private fun initDataBinding(){
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = MainViewModel()
+        dataBinding.lifecycleOwner = this
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initDataBinding()
     }
+
 }
